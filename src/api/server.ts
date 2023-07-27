7e17958e9316d468ce7236f54d28629ad97badb1
purplerising <123110972+purplerising@users.noreply.github.com>
2023-07-27 17:52:52 -0400

    Try Again

--- src/api/server.ts
+++ src/api/server.ts
@@ -2,9 +2,7 @@ const token = '002def80ca63b60610b65ed0688d72cd1c0364f7ab43e5c7'
 
 export const server_calls = {
     get: async () => {
-        const response = await fetch(`https://flasktrial3.onrender.com/api/cars`,
-        // change url to my url once set up
-        {
+        const response = await fetch(`https://flasktrial3.onrender.com/api/cars`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
@@ -21,8 +19,7 @@ export const server_calls = {
     },
 
     create: async (data: any = {}) => {
-        const response = await fetch(`https://flasktrial3.onrender.com/api/cars`,
-        {
+        const response = await fetch(`https://flasktrial3.onrender.com/api/cars`, {
             method: "POST",
             headers: {
                 'Content-Type': 'application/json',
@@ -30,7 +27,7 @@ export const server_calls = {
                 'Access-Control-Allow-Origin': '*',
             },
             body: JSON.stringify(data)
-        })
+        });
 
         if (!response.ok) {
             throw new Error('Failed to create new data on the server')
@@ -39,35 +36,33 @@ export const server_calls = {
         return await response.json()
     },
 
-    update: async (id:string, data: any = {}) => {
-        const response = await fetch(`https://flasktrial3.onrender.com/api/cars/${id}`,
-        {
-            method: "POST",
+    update: async (id: string, data: any = {}) => {
+        const response = await fetch(`https://flasktrial3.onrender.com/api/cars/${id}`, {
+            method: "PUT", // changed from "POST" to "PUT"
             headers: {
                 'Content-Type': 'application/json',
                 'x-access-token': `Bearer ${token}`,
                 'Access-Control-Allow-Origin': '*',
             },
             body: JSON.stringify(data)
-        })
+        });
 
-        if (!response.ok){
+        if (!response.ok) {
             throw new Error('Failed to update data on server')
         }
 
         return await response.json()
     },
 
-    delete: async (id:string) => {
-        const response = await fetch(`https://flasktrial3.onrender.com/api/cars/${id}`,
-        {
+    delete: async (id: string) => {
+        const response = await fetch(`https://flasktrial3.onrender.com/api/cars/${id}`, {
             method: "DELETE",
             headers: {
                 'Content-Type': 'application/json',
                 'x-access-token': `Bearer ${token}`,
                 // 'Access-Control-Allow-Origin': '*',
             }
-        })
+        });
 
         if (!response.ok){
             throw new Error('Failed to delete data on server')
--- src/components/DataTable.tsx
+++ src/components/DataTable.tsx
@@ -2,12 +2,9 @@ import React, { useState } from 'react'
 import Button from './Button'
 import Modal from './Modal'
 import { server_calls } from '../api/server';
-import { DataGrid, GridColDef} from '@mui/x-data-grid';
-// import Box from '@mui/material/Box';
-
-
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import { useGetData } from '../custom-hooks/FetchData';
-import { Box, Typography } from '@mui/material';
+import { Box } from '@mui/material';
 
 const columns: GridColDef[] = [
   { field: 'vin', headerName: 'VIN', flex: 1 },
@@ -18,10 +15,9 @@ const columns: GridColDef[] = [
 ]
 
 function DataTable() {
-  const [ open, setOpen ] = useState(false);
+  const [open, setOpen] = useState(false);
   const { carData, getData } = useGetData();
-  const [ selectionModel, setSelectionModel ] = useState<string[]>([])
-  
+  const [selectionModel, setSelectionModel] = useState<string[]>([])
   
   const handleOpen = () => {
     setOpen(true)
@@ -31,12 +27,13 @@ function DataTable() {
     setOpen(false)
   }
   
-
   const deleteData = () => {
     server_calls.delete(selectionModel[0]);
-    getData();
-    console.log(`Selection model: ${selectionModel[1]}`)
-    setTimeout( () => { window.location.reload() }, 500000000000000)
+    console.log(`Deleted: ${selectionModel[0]}`)
+    setTimeout(() => { 
+      getData();
+      window.location.reload() 
+    }, 1000)
   }
 
   return (
@@ -45,7 +42,6 @@ function DataTable() {
             id={selectionModel}
             open={open}
             onClose={handleClose}/>
-              
         <div className="flex flex-row p-3 justify-items-center bg-slate-300 m-3 rounded hover:bg-slate-700 hover:text-white">
             <div className="flex flex-row">
                 <button
@@ -62,29 +58,17 @@ function DataTable() {
           style={{ height: 500, width: '100%' }}
           >
             <h2 className="flex p-3 bg-slate-300 justify-items-center items-center h-auto w-auto  my-2 rounded">My Cars</h2>
-            {/* <Box  
-            sx={{
-              boxShadow: 2,
-              border: 2,
-              borderColor: 'primary.light',
-              '& .MuiDataGrid-cell:hover': {
-                color: 'primary.main',
-              },
-            }} */}
-            {/* > */}
             <DataGrid autoHeight {...carData}
               rows={carData} 
               columns={columns}  
               checkboxSelection={true}
-              getRowId={(row) => row.id + row.vin}
-              onRowSelectionModelChange={ (item:any) => {setSelectionModel(item)
-              }}
+              getRowId={(row) => row.vin}
+              onRowSelectionModelChange={ (item:any) => {setSelectionModel(item.selectionModel)}
+              }
             />
-            
-            {/* </Box> */}
         </div>
     </>
   )
 }
 
-export default DataTable
+export default DataTable;
