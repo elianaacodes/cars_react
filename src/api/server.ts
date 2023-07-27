0c10e1a7f41f2844b35849b6cdf1425805471b12
purplerising <123110972+purplerising@users.noreply.github.com>
2023-07-27 14:13:56 -0400

    fourth commit

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,16 +1,19 @@
 const token = '06a146580fbffe6e6e9aee9bd8ae9b53da7f293b57cc4bf3'
 
+// const token = '27f4d250d324ba15d3f9e28b7d0998ef80954c8a63bdcde0'
+// if using flask token enter above and modify links below
+
 export const server_calls = {
-    get: async () => { 
+    get: async () => {
         const response = await fetch(`https://flasktrial3.onrender.com/cars`,
+        // change url to my url once set up
         {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`,
                 'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`
-            }
-
+            },
         });
 
         if (!response.ok){
@@ -19,5 +22,60 @@ export const server_calls = {
 
         return await response.json()
     },
-    
+
+    create: async (data: any = {}) => {
+        const response = await fetch(`https://flasktrial3.onrender.com/cars`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`,
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok) {
+            throw new Error('Failed to create new data on the server')
+        }
+
+        return await response.json()
+    },
+
+    update: async (id:string, data: any = {}) => {
+        const response = await fetch(`https://flasktrial3.onrender.com/cars/${id}`,
+        {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`,
+                'Access-Control-Allow-Origin': '*',
+            },
+            body: JSON.stringify(data)
+        })
+
+        if (!response.ok){
+            throw new Error('Failed to update data on server')
+        }
+
+        return await response.json()
+    },
+
+    delete: async (id:string) => {
+        const response = await fetch(`https://flasktrial3.onrender.com/cars${id}`,
+        {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+                'x-access-token': `Bearer ${token}`,
+                // 'Access-Control-Allow-Origin': '*',
+            }
+        })
+
+        if (!response.ok){
+            throw new Error('Failed to delete data on server')
+        }
+
+        return;
+    },
 }
