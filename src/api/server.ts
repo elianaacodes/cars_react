065d1125026b5ff7c365f40d9b7cfb1d09cfb2b2
purplerising <123110972+purplerising@users.noreply.github.com>
2023-07-27 14:06:57 -0400

    third commit

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,17 +1,16 @@
 const token = '06a146580fbffe6e6e9aee9bd8ae9b53da7f293b57cc4bf3'
-// if using flask token enter above and modify links below
 
 export const server_calls = {
-    get: async () => {
-        const response = await fetch(`https://lateral-quirky-piccolo.glitch.me/api/cars`,
-        // change url to my url once set up
+    get: async () => { 
+        const response = await fetch(`https://flasktrial3.onrender.com/cars`,
         {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
                 'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`,
-            },
+                'x-access-token': `Bearer ${token}`
+            }
+
         });
 
         if (!response.ok){
@@ -20,59 +19,5 @@ export const server_calls = {
 
         return await response.json()
     },
-
-    create: async (data: any = {}) => {
-        const response = await fetch(`https://lateral-quirky-piccolo.glitch.me/api/cars`,
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'access-control-allow-origin':	'*',
-                'x-access-token': `Bearer ${token}`,
-            },
-            body: JSON.stringify(data)
-        })
-
-        if (!response.ok) {
-            throw new Error('Failed to create new data on the server')
-        }
-
-        return await response.json()
-    },
-
-    update: async (id:string, data: any = {}) => {
-        const response = await fetch(`https://lateral-quirky-piccolo.glitch.me/api/cars/${id}`,
-        {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Origin': '*',
-                'x-access-token': `Bearer ${token}`,
-            },
-            body: JSON.stringify(data)
-        })
-
-        if (!response.ok){
-            throw new Error('Failed to update data on server')
-        }
-
-        return await response.json()
-    },
-
-    delete: async (id:string) => {
-        const response = await fetch(`https://lateral-quirky-piccolo.glitch.me/api/cars/${id}`,
-        {
-            method: "DELETE",
-            headers: {
-                'Content-Type': 'application/json',
-                'x-access-token': `Bearer ${token}`,
-            }
-        })
-
-        if (!response.ok){
-            throw new Error('Failed to delete data on server')
-        }
-
-        return;
-    },
+    
 }
